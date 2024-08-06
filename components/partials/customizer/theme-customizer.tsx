17007b353e2e9f4e743e3ebae3cf313b51fd661c
Joel Genaro <joel.ggdev@gmail.com>
2024-08-06 17:31:54 +0900

    fixed ltr:right-4 rtl:left-4 bottom-14 z-50

--- components/partials/customizer/theme-customizer.tsx
+++ components/partials/customizer/theme-customizer.tsx
@@ -28,8 +28,8 @@ import { useThemeStore } from "@/store";
 
 const ThemeCustomize = ({
   trigger = (
-    <div className="fixed ltr:right-4 rtl:left-4 bottom-14 z-50">
-      <Button size="icon" className=" relative h-12 w-12  rounded-full ">
+    <div className="fixed z-50 hidden ltr:right-4 rtl:left-4 bottom-14">
+      <Button size="icon" className="relative w-12 h-12 rounded-full ">
         <Settings className="h-7 w-7 animate-spin" />
       </Button>
     </div>
@@ -43,15 +43,15 @@ const ThemeCustomize = ({
       <SheetContent
         side={isRtl ? "left" : "right"}
         overlayClass=" bg-transparent backdrop-blur-none"
-        className="lg:w-3/4 w-full max-w-full md:max-w-sm px-6 pt-0 pb-6"
+        className="w-full max-w-full px-6 pt-0 pb-6 lg:w-3/4 md:max-w-sm"
       >
-        <SheetHeader className=" text-start border-b -mx-6 px-6 py-4 shadow-sm md:shadow-none">
-          <SheetTitle className=" text-base  font-medium ">
+        <SheetHeader className="px-6 py-4 -mx-6 border-b shadow-sm  text-start md:shadow-none">
+          <SheetTitle className="text-base font-medium ">
             Theme Customizer
           </SheetTitle>
         </SheetHeader>
         <ScrollArea className="h-[calc(100%-120px)] -mx-6 px-6">
-          <div className=" space-y-8 mt-3">
+          <div className="mt-3 space-y-8 ">
             <SelectLayout />
             <SelectTheme />
             <RtlSwitcher />
@@ -63,7 +63,7 @@ const ThemeCustomize = ({
             <FooterStyle />
           </div>
         </ScrollArea>
-        <SheetFooter className="py-4 gap-3 lg:flex justify-between hidden  ">
+        <SheetFooter className="justify-between hidden gap-3 py-4 lg:flex ">
           <Button asChild className="w-full">
             <Link href="https://1.envato.market/vNaJR3">Buy Now</Link>
           </Button>
