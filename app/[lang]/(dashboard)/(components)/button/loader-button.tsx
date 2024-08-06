cf52a82efb14c4e0fb8fa25ad06acfadbeffa249
Joel Genaro <joel.ggdev@gmail.com>
2024-08-06 16:39:14 +0900

    update logo

--- app/[lang]/(dashboard)/(components)/button/loader-button.tsx
+++ app/[lang]/(dashboard)/(components)/button/loader-button.tsx
@@ -4,27 +4,27 @@ const LoaderButton = () => {
   return (
     <>
       <Button disabled>
-        <Loader2 className="ltr:mr-2 rtl:ml-2 h-4 w-4 animate-spin" />
+        <Loader2 className="w-4 h-4 ltr:mr-2 rtl:ml-2 animate-spin" />
         Loading ...
       </Button>
       <Button color="secondary" disabled>
-        <Loader2 className="ltr:mr-2 rtl:ml-2 h-4 w-4 animate-spin" />
+        <Loader2 className="w-4 h-4 ltr:mr-2 rtl:ml-2 animate-spin" />
         Loading ...
       </Button>
       <Button color="success" disabled>
-        <Loader2 className="ltr:mr-2 rtl:ml-2 h-4 w-4 animate-spin" />
+        <Loader2 className="w-4 h-4 ltr:mr-2 rtl:ml-2 animate-spin" />
         Loading ...
       </Button>
       <Button color="info" disabled>
-        <Loader2 className="ltr:mr-2 rtl:ml-2 h-4 w-4 animate-spin" />
+        <Loader2 className="w-4 h-4 ltr:mr-2 rtl:ml-2 animate-spin" />
         Loading ...
       </Button>
       <Button color="warning" disabled>
-        <Loader2 className="ltr:mr-2 rtl:ml-2 h-4 w-4 animate-spin" />
+        <Loader2 className="w-4 h-4 ltr:mr-2 rtl:ml-2 animate-spin" />
         Loading ...
       </Button>
       <Button color="destructive" disabled>
-        <Loader2 className="ltr:mr-2 rtl:ml-2 h-4 w-4 animate-spin" />
+        <Loader2 className="w-4 h-4 ltr:mr-2 rtl:ml-2 animate-spin" />
         Loading ...
       </Button>
     </>
--- components/layout-loader.tsx
+++ components/layout-loader.tsx
@@ -1,13 +1,20 @@
 "use client";
 import React from "react";
-import { SiteLogo } from "@/components/svg";
 import { Loader2 } from "lucide-react";
+import logo from "@/public/images/logo/logo-1.png"
+import {
+  Img,
+} from "@react-email/components";
+
 const LayoutLoader = () => {
   return (
-    <div className=" h-screen flex items-center justify-center flex-col space-y-2">
-      <SiteLogo className=" h-10 w-10 text-primary" />
-      <span className=" inline-flex gap-1">
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+    <div className="flex flex-col items-center justify-center h-screen space-y-2 ">
+      <Img className="h-10 text-primary"
+        src={logo.src}
+        alt="DashTail"
+      />
+      <span className="inline-flex gap-1 ">
+        <Loader2 className="w-4 h-4 mr-2 animate-spin" />
         Loading...
       </span>
     </div>
