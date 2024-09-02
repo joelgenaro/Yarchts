476ae2518277b813bf823d15913f84a46b4a8dba
Joel Genaro <joel.ggdev@gmail.com>
2024-09-02 15:51:33 +0900

    our fence db migration

---
+++ db/migrations/0005_thin_nick_fury.sql
@@ -0,0 +1,15 @@
+ALTER TABLE "fences" ADD COLUMN "our_panel_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_post_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_lft_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_3_feet_gate_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_4_feet_gate_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_5_feet_gate_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_8_feet_gate_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_10_feet_gate_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_heavy_duty_end_post_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_end_post_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_corner_post_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_flat_cap_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_gothic_cap_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_new_england_cap_price" numeric;--> statement-breakpoint
+ALTER TABLE "fences" ADD COLUMN "our_federation_cap_price" numeric;
